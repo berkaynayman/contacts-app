fe5bb09939d1ae89b5695fc13d3249aba7ab47ab
berkaynayman <berkaynayman4@gmail.com>
2022-01-26 21:55:44 +0300

    add Redux, createEntityAdaptor

---
+++ src/components/Contacts/Form.js
@@ -0,0 +1,26 @@
+import {useState} from 'react';
+import { nanoid } from '@reduxjs/toolkit';
+import {useDispatch} from 'react-redux'
+import {addContact} from '../../redux/contactSlice'
+
+function Form() {
+    const [name, setName] = useState("")
+    const dispatch = useDispatch()
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(!name) return false
+        dispatch(addContact({ id: nanoid(), name}))
+        setName("")
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input placeholder='name' value={name} onChange={(e) => setName(e.target.value)} />
+            </form>        
+        </div>
+        
+    );
+}
+
+export default Form;
--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -1,36 +1,11 @@
-import { useState, useEffect } from "react"
-import List from "./List"
-import Form from "./Form"
+import React from 'react';
+import Form from './Form';
 
-import "./style.css"
-
-function Contacts(){
-    const [contacts, setContacts] = useState([
-        {
-            fullname: "Berkay Nayman",
-            phone_number: "555 1234 4545"
-        },
-        {
-            fullname: "Fatma Aslan",
-            phone_number: "533 2341 1122"
-        },
-        {
-            fullname: "Mehmet",
-            phone_number: "561 6161 6161"
-        }
-    ])
-    
-    useEffect(() => {
-        console.log(contacts);
-    }, [contacts]);
-
-    return(
-        <div id="container">
-            <h1>Contacts</h1>
-            <List contacts={contacts}/>
-            <Form addContact={setContacts} contacts={contacts}/>
-        </div>
-    );
+function Contacts() {
+  return <div>
+      <h1>Contacts</h1>
+      <Form />
+  </div>;
 }
 
 export default Contacts;
